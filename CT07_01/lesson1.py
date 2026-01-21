f04f8eefbf0d5a03b4a34f87a4b85e0b125111af
Walden-pro <waldenchho@gmail.com>
2026-01-21 09:00:41 +0000

    lesson3

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,53 @@
-print("Hello from lesson 1")
+# print("Hello from lesson 1")
+
+
+
+# set a variable "glass" and other variables for plastic and paper for the bins.
+# check if the item for recycling is glass, plastic or paper.
+# if item = glass
+#  print glass
+# if item = glass, 
+# put in the glass bin.
+# repeat until no more items
+# put the item according to the bins for glass, plastic and paper
+
+
+
+# red = 1;
+# blue = 2;
+# green = 3;
+
+# total = (red*3)+ (blue*5)+(green*4);
+# print("you spent $" + str(total)+ " today");
+
+
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+
+
+score = input("Input score: ")
+
+if score >= 75:
+    print("You got grade A")
+
+elif score >= 60:
+    print("You got grade B")
+elif score >= 50:
+    print("You got grade C")
+else: 
+    print("You failed")
+
+
+
 

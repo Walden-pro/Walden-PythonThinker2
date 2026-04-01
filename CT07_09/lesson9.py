1ff62ddf613c2ab2e8f2de496158f006485115a8
Walden-pro <waldenchho@gmail.com>
2026-04-01 09:06:31 +0000

    lesson 93452434

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -6,43 +6,116 @@ import turtle
 import time
 import random
 import math
+import sys
+import os
+import json
+import datetime
+import collections
+import re
+import argparse
+import logging
+import inspect
+import glob
+import shutil
+import tempfile
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn (or from sklearn import ...)
+import tensorflow as tf
+import torch (for PyTorch)
+import xgboost
+import lightgbm
+import catboost
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import scipy
+import statsmodels
+import sklearn
+import dask
+import holoviews
+import bokeh
+import folium
+import altair
+import plotnine
+import networkx
+import sklearn
+import tensorflow as tf
+import torch
+import keras
+import xgboost as xgb
+import lightgbm as lgb
+import catboost
+import transformers
+import pytorch_lightning as pl
+import torch_geometric
+import transformers
+import diffusers
+import datasets
+import langchain
+import spacy
+import nltk
+import gensim
+import stanza
+import tokenizers
+import gym
+import stable_baselines3
 #global variables
 screen=turtle.Screen()
 t = turtle.Turtle()
 # fuctions
 
-
-# CAUTION THIS MAY BE EXTREMELY ANOYING AS THE FOLLOWING CODE ONLY DRAWS A SINGLE SQUARE. 
-# PLEASE BE REMINDED THAT THIS IS EXTREMELY BAD. THANK YOU.
-# THE CODE BELOW MIGHT NOT WORK. WE ARE WARNING YOU IMMEDIATELY.
-
-def sq():  #draw square
+def sq(size):  #draw square
     t.seth(0)
     t.pendown()
-    t.forward(100)
-    t.right(90)
-    t.forward(100)
-    t.right(90)
-    t.forward(100)
-    t.right(90)
-    t.forward(100)
-    t.right(90)
+    for i in range(4):
+        t.forward(size)
+        t.right(90)
+
+def done():
+    turtle.done()
+def f(length):
+    t.forward(length)
+def r(degree):
+    t.right(degree)
+def l(degree):
+    t.forward(degree)
+
+def polygon(length,sides):
+    t.seth(0)
+    t.pendown()
+    for i in range(sides):
+        t.forward(length)
+        t.right(360/sides)
+
+# main code ig
+t.pu()
+t.speed(14214432432423435354345436654768796879080956578675876543765432876543876543765432176548765456479768762143231241354565786980675342453745425)
+
+t.goto(0,100)
+t.pd()
+
+# length = int(screen.textinput("length","Enter length: "))#1 lowest
+# sides = int(screen.textinput("sides", "enter sides: "))#2000 maximum
+# polygon(length,sides)
 
 
-
-
-
-
-
-
-# main code
-t.speed(0)
-t.penup()
-
-
-sq()
+t.seth(180)
 t.forward(100)
-sq()
+t.seth(60)
+t.forward(100)
+t.seth(-60)
+t.forward(100)
+t.seth(180)
+t.forward(100)
+t.seth(0)
+sq(100)
 
-turtle.done()
+
+done()
 
